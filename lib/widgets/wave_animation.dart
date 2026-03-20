c397506eb932ffb927413e7a1fb8b6a51f9b36bc
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-20 11:35:22 +0000

    feat: implement wave generation logic and AnimatedBuilder

--- lib/widgets/wave_animation.dart
+++ lib/widgets/wave_animation.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math.dart' as vector;
 
 class WaveClipper extends CustomClipper<Path> {
   final double animation;
@@ -62,6 +64,34 @@ class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Container(
+      alignment: Alignment.bottomCenter,
+      child: AnimatedBuilder(
+        animation: CurvedAnimation(
+          parent: animationController,
+          curve: Curves.easeInOut,
+        ),
+        builder: (context, child) {
+          // Generate wave points inside the builder for cleaner reactivity
+          List<Offset> animList1 = [];
+          for (int i = -2 - widget.xOffset; i <= widget.size.width.toInt() + 2; i++) {
+            animList1.add(
+              Offset(
+                i.toDouble() + widget.xOffset,
+                sin((animationController.value * 360 - i) % 360 * vector.degrees2Radians) * 10 + 30 + widget.yOffset,
+              ),
+            );
+          }
+
+          return ClipPath(
+            clipper: WaveClipper(animationController.value, animList1),
+            child: Container(
+              width: widget.size.width,
+              height: widget.size.height,
+              color: widget.color,
+            ),
+          );
+        },
+      ),
     );
   }
 }
