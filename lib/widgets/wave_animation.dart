2418b211ad17c79b05d17c1aed0721b6e3222f4e
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-20 11:25:23 +0000

    feat: add WaveClipper boilerplate for wave animation

---
+++ lib/widgets/wave_animation.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class WaveClipper extends CustomClipper<Path> {
+  final double animation;
+  final List<Offset> waveList1;
+
+  WaveClipper(this.animation, this.waveList1);
+}
