723df458383948a38d892d9eba6bee8f69821a6e
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-20 11:18:33 +0000

    feat: add boilerplate for RoundedButton widget

---
+++ lib/widgets/rounded_button_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatefulWidget {
+  final String text;
+
+  const RoundedButton({super.key, required this.text});
+
+  @override
+  State<RoundedButton> createState() => _RoundedButtonState();
+}
+
+class _RoundedButtonState extends State<RoundedButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
