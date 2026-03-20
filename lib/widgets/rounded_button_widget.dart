f56d1af53f2f1f5b80acbc0cffc45c8b922ef0aa
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-20 11:19:05 +0000

    feat: implement UI styling for RoundedButton

--- lib/widgets/rounded_button_widget.dart
+++ lib/widgets/rounded_button_widget.dart
@@ -13,6 +13,26 @@ class _RoundedButtonState extends State<RoundedButton> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 140.0,
+      height: 140.0,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 5.0
+            )
+          ]
+      ),
+      child: Center(
+        child: Text(
+          widget.text,
+          style: const TextStyle(
+              fontSize: 20.0,
+          ),
+        ),
+      ),
     );
   }
 }
