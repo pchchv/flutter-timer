639f4ef3b43939ad348df8bcbef9615ce60ce340
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-23 18:56:35 +0000

    feat: add Timer and Stopwatch initialization to TimerPage

--- lib/pages/timer.dart
+++ lib/pages/timer.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_timer/model/task.dart';
 
@@ -7,5 +8,19 @@ class TimerPage extends StatefulWidget {
   const TimerPage({Key? key, required this.task}) : super(key: key);
 
   @override
-  ()=>();
+  _TimerPageState createState() => _TimerPageState();
+}
+
+class _TimerPageState extends State<TimerPage> with SingleTickerProviderStateMixin {
+  late Timer timer;
+  final Stopwatch stopwatch = Stopwatch();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+      ),
+    );
+  }
 }
