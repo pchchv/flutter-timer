b7cfb0f92c104ffc4893ebf671c3cd7d0ce576bf
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-24 14:27:46 +0000

    feat: add duration formatting logic to TaskWidget

--- lib/widgets/task.dart
+++ lib/widgets/task.dart
@@ -7,6 +7,13 @@ class TaskWidget extends StatelessWidget {
 
   final Task task;
 
+  String _formatDuration() {
+    final h = task.hours.toString().padLeft(2, '0');
+    final m = task.minutes.toString().padLeft(2, '0');
+    final s = task.seconds.toString().padLeft(2, '0');
+    return '$h:$m:$s';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
