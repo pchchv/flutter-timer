a8b8b71ec835bf38876c62b038fd1c73e4d2e954
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-24 14:27:43 +0000

    feat: implement build method for TaskWidget

--- lib/widgets/task.dart
+++ lib/widgets/task.dart
@@ -6,4 +6,10 @@ class TaskWidget extends StatelessWidget {
   const TaskWidget({Key? key, required this.task}) : super(key: key);
 
   final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
 }
