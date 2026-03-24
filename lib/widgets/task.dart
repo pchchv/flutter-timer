a9a02b434d0c87442e934395c424c1e8df0c3755
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-24 14:26:01 +0000

    feat: create TaskWidget with task model dependency

---
+++ lib/widgets/task.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_timer/model/task.dart';
+
+class TaskWidget extends StatelessWidget {
+  const TaskWidget({Key? key, required this.task}) : super(key: key);
+
+  final Task task;
+}
