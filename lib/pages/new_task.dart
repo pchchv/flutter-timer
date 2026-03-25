8f2ba50721b3dd9b2150b9bece95735a8f8c85e4
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-25 08:30:13 +0000

    feat: add items and callbacks to _Selector widget

--- lib/pages/new_task.dart
+++ lib/pages/new_task.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 class _Selector<T> extends StatefulWidget {
+  final List<T> items;
+  final String Function(T) itemBuilder;
+  final Function(T) onSelectedItemChanged;
+
+  const _Selector({
+    super.key,
+    required this.items,
+    required this.itemBuilder,
+    required this.onSelectedItemChanged,
+  });
 
   @override
   _SelectorState<T> createState() => _SelectorState<T>();
