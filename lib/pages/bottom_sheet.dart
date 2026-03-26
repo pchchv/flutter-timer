7ad7d68de3811036db20dfc01051e6feff4f90c6
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-26 09:38:31 +0000

    feat: add AnimationController and factory method to CustomBottomSheet

--- lib/pages/bottom_sheet.dart
+++ lib/pages/bottom_sheet.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 
+const Duration _kBottomSheetDuration = Duration(milliseconds: 200);
+
 class CustomBottomSheet extends StatefulWidget {
+  const CustomBottomSheet({
+    super.key,
+    this.animationController,
+    required this.onClosing,
+    required this.builder,
+  });
+
+  final AnimationController? animationController;
+  final VoidCallback onClosing;
+  final WidgetBuilder builder;
+
   @override
   State<CustomBottomSheet> createState() => _CustomBottomSheetState();
+
+  static AnimationController createAnimationController(TickerProvider vsync) {
+    return AnimationController(
+      duration: _kBottomSheetDuration,
+      debugLabel: 'BottomSheet',
+      vsync: vsync,
+    );
+  }
 }
 
 class _CustomBottomSheetState extends State<CustomBottomSheet> {
