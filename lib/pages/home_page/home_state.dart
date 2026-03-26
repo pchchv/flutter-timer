6dc62777afdf1a0c07b4a4b245dc355eee655eea
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-26 09:46:03 +0000

    feat: implement HomeStateLoaded with task list and props

--- lib/pages/home_page/home_state.dart
+++ lib/pages/home_page/home_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_timer/model/task.dart';
 
 abstract class HomeState extends Equatable {
   const HomeState();
@@ -12,4 +13,16 @@ class HomeStateLoading extends HomeState {
 
   @override
   String toString() => 'HomeStateLoading';
+}
+
+class HomeStateLoaded extends HomeState {
+  final List<Task> tasks;
+
+  const HomeStateLoaded({required this.tasks});
+
+  @override
+  List<Object?> get props => [tasks];
+
+  @override
+  String toString() => 'HomeStateLoaded { tasks: ${tasks.length} }';
 }
