2dafb6992ad0f2cb68fd403e2c68b4c357319ff4
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-26 09:40:55 +0000

    feat: implement drag update handling and child height calculation

--- lib/pages/bottom_sheet.dart
+++ lib/pages/bottom_sheet.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Duration _kBottomSheetDuration = Duration(milliseconds: 200);
+// const double _kMinFlingVelocity = 700.0;
+// const double _kCloseProgressThreshold = 0.5;
 
 class CustomBottomSheet extends StatefulWidget {
   const CustomBottomSheet({
@@ -27,6 +29,22 @@ class CustomBottomSheet extends StatefulWidget {
 }
 
 class _CustomBottomSheetState extends State<CustomBottomSheet> {
+  final GlobalKey _childKey = GlobalKey(debugLabel: 'BottomSheet child');
+
+  double get _childHeight {
+    final RenderBox? renderBox = _childKey.currentContext?.findRenderObject() as RenderBox?;
+    return renderBox?.size.height ?? 0.0;
+  }
+
+  bool get _dismissUnderway => widget.animationController?.status == AnimationStatus.reverse;
+
+  void _handleDragUpdate(DragUpdateDetails details) {
+    if (_dismissUnderway || widget.animationController == null) return;
+    
+    double delta = details.primaryDelta ?? 0.0;
+    widget.animationController!.value -= delta / (_childHeight > 0 ? _childHeight : delta);
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
