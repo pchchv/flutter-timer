e2b4bb507ef90be6428907452531aeca04a11d8e
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-26 09:37:21 +0000

    feat: create CustomBottomSheet widget and state

---
+++ lib/pages/bottom_sheet.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomSheet extends StatefulWidget {
+  @override
+  State<CustomBottomSheet> createState() => _CustomBottomSheetState();
+}
+
+class _CustomBottomSheetState extends State<CustomBottomSheet> {
+}
