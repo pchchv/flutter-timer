a062d5d26bb0503e1eb5df0fcfdc02b218593935
Evgeni Pochechuev <ipchchv@gmail.com>
2026-03-27 05:46:02 +0000

    feat: implement addNewTask method in TaskManager

--- lib/data/task_manager.dart
+++ lib/data/task_manager.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
+import 'package:flutter_timer/model/task.dart';
 import 'package:flutter_timer/data/database.dart';
 
 class TaskManager {
   final DatabaseProvider dbProvider;
 
   const TaskManager({required this.dbProvider});
+
+  Future<int> addNewTask(Task task) async {
+    return await dbProvider.insert(task);
+  }
 }
